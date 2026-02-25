954ea93b8d9d5a385ec4e7046339b9d153bef559
AmearShaik <amearshaik11@gmail.com>
2026-02-25 15:10:41 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-
-class Calculator {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(a + b); // addition
-        System.out.println(a - b); // subtraction
-        System.out.println(a * b); // multiplication
-        System.out.println(a / b); // division
-    }
+class Calculator
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+int n2=Integer.parseInt(args[1]);
+int r1 = n1+n2;
+int r2 = n1-n2;
+int r3 = n1*n2;
+int r4 = n1/n2;
+System.out.println("Addition: "+r1);
+System.out.println("Subtraction: "+r2);
+System.out.println("Multiplication: "+r3);
+System.out.println("Division: "+r4);
+}
 }
